cb528c0c62bd93a142ebbfa406137ed0f3abd195
Matin Amiri <matinpc.a@gmail.com>
2024-09-27 13:56:34 +0330

    add font

---
+++ src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header , Footer} from '../../components';
+
+const AboutUs = () => {
+    return (
+        <>
+            <Header />
+            AboutUs
+            <Footer />
+        </>
+    );
+}
+
+export default AboutUs;
---
+++ src/pages/productList/ProductList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header , Footer} from '../../components';
+
+const ProductList = () => {
+    return (
+        <>
+            <Header />
+            ProductList
+            <Footer />
+        </>
+    );
+}
+
+export default ProductList;
